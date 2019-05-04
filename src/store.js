5f45206460c86f91521c53cb2cad481a429a8784
Patrick Santos <patrickspteixeira@gmail.com>
2019-05-04 00:21:12 -0300

    add solution

--- src/store.js
+++ src/store.js
@@ -4,7 +4,37 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    places: [],
+    cart: [],
+    search: ""
+  },
+  mutations: {
+    setPlaces(state, places) {
+      state.places = places;
+    },
+    addToCart(state, newItem) {
+      state.cart = [...state.cart, newItem];
+    },
+    setSearch(state, search) {
+      state.search = search;
+    }
+  },
+  actions: {
+    addToCart({ commit, state }, { item, place }) {
+      if (state.cart.some(order => order.id === item.id)) return;
+
+      const newItem = { ...item, place };
+      commit("addToCart", newItem);
+    }
+  },
+  getters: {
+    filteredList({ places, search }) {
+      return search
+        ? places.filter(item =>
+            item.title.toLowerCase().includes(search.toLowerCase().trim())
+          )
+        : places;
+    }
+  }
 });
